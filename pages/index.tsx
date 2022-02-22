a2426aa51d68f7a75939f7196acc072c5f00893d
Víctor Rico <vricop@gmail.com>
2022-02-22 10:38:45 +0100

    Add generic type back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,29 @@
-import HolidaysCalendar from '@/components/HolidaysCalendar'
-import { EmployeeDetails } from '@types'
+import { HolidaysCalendar } from '@/components/HolidaysCalendar'
+import { Icon } from '@/components/Icon'
+import { CalendarItem, Employee, EmployeeDetails } from '@types'
 import { useFetch } from 'hooks/fetchData'
 import type { NextPage } from 'next'
 
+type CalendarData = CalendarItem[] | null
+type EmployeeData = EmployeeDetails[] | null
+
 const Home: NextPage = () => {
-  const {
-    data,
-    loading,
-  }: {
-    data: EmployeeDetails[] | null
-    loading: boolean
-  } = useFetch(`/api/employees`)
+  const { data: employees, loading: loadingEmployees } =
+    useFetch<EmployeeData>(`/api/employees`)
+
+  const { data: calendar, loading: loadingCalendar } =
+    useFetch<CalendarData>(`/api/calendar`)
+  const filteredCalendar = calendar?.filter(({ fecha }) =>
+    /2021\d{2}\d{2}/.test(fecha.toString())
+  )
 
   return (
     <main className="container">
-      {loading ? 'Loading' : <HolidaysCalendar employees={data} />}
+      {loadingEmployees ? (
+        'Loading'
+      ) : (
+        <HolidaysCalendar employees={employees} />
+      )}
     </main>
   )
 }
