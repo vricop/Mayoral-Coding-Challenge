38e6843f4aa56aea77983a231ea3e41361545d2c
Víctor Rico <vricop@gmail.com>
2022-02-22 17:36:49 +0100

    Add groupInMounths helper function

---
+++ utils/groupInMonths.ts
@@ -0,0 +1,34 @@
+import { CalendarItem, DayOfMonth, DayType, DayTypeID } from '@types'
+
+export function groupInMounts(months: CalendarItem[] | undefined) {
+  if (months === undefined) return []
+
+  let monthName: string, day: number, date: Date
+  let dayObject: { day: number; type: DayType; id: DayTypeID }
+  const dateRegex = /(\d{4})(\d{2})(\d{2})/
+
+  const groupedMonths = months?.reduce(
+    (result, { fecha, tipoDs: type, tipoId: id }: CalendarItem) => {
+      date = new Date(fecha.toString().replace(dateRegex, '$1-$2-$3'))
+      monthName = date.toLocaleString('default', { month: 'long' })
+      day = date.getDate()
+
+      dayObject = {
+        day,
+        type,
+        id,
+      }
+
+      if (result.has(monthName)) {
+        result?.get(monthName)?.push(dayObject)
+      } else {
+        result.set(monthName, [dayObject])
+      }
+
+      return result
+    },
+    new Map<string, DayOfMonth[]>()
+  )
+
+  return Array.from(groupedMonths?.entries())
+}
